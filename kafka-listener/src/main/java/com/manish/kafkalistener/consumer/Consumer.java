619f5940d121fd13926fbd550a72168c3b202fa3
manish7213 <manish.1si11cs057@gmail.com>
2020-02-25 12:45:27 +0530

    Removed hardcoding from config files

--- kafka-listener/src/main/java/com/manish/kafkalistener/consumer/Consumer.java
+++ kafka-listener/src/main/java/com/manish/kafkalistener/consumer/Consumer.java
@@ -1,7 +1,9 @@
 package com.manish.kafkalistener.consumer;
 
+import com.manish.kafkalistener.configuration.YAMLConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -21,22 +23,22 @@ import java.util.Date;
 public class Consumer {
 
     private final Logger LOGGER = LoggerFactory.getLogger(Consumer.class);
-    private static final String basePath = "C:/Users/manishkumar75/Desktop/BatchFiles/";
-
+    @Autowired
+    private YAMLConfiguration config;
     @KafkaListener(topics = "test", groupId = "group_id")
     public void consume(String message) {
         LOGGER.info("Consumed Message is {} ",message);
         PrintWriter out = null;
         try {
             String fileName = new SimpleDateFormat("yyyyMMddHHmmss'.csv'").format(new Date());
-            File file = new File(basePath+fileName);
+            File file = new File(config.getOutputFileBaseLocation()+fileName);
             file.createNewFile();
             out = new PrintWriter(new BufferedWriter(new FileWriter(file, true)));
             out.println(message);
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            if (out != null){
+            if (null != out){
                 out.flush();
                 out.close();
             }
--- kafka-listener/src/main/java/com/manish/kafkalistener/producer/Producer.java
+++ kafka-listener/src/main/java/com/manish/kafkalistener/producer/Producer.java
@@ -1,5 +1,6 @@
 package com.manish.kafkalistener.producer;
 
+import com.manish.kafkalistener.configuration.YAMLConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,6 @@ import java.io.*;
 public class Producer {
     private static final Logger logger = LoggerFactory.getLogger(Producer.class);
     private static final String TOPIC = "test";
-
     @Autowired
     private KafkaTemplate<String,String> kafkaTemplate;
     public void sendMessage(String message){
